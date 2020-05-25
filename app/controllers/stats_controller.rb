b1b2dd3834385783b54da1847a809eeb0c7e93cb
Ernest_k <ekhasanzhinov@techmategroup.com>
2020-05-25 17:26:41 +0300

    create controllers

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def show
+
+  end
+
+  def search
+
+  end
+end
---
+++ app/controllers/trades_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class TradesController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def erase
+
+  end
+end
